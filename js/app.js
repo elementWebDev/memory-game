ceca94956d1ce3563db3eb54ef9d1d3247be746d
elementWebDev <ivinci.design@gmail.com>
2018-08-04 16:31:03 -0700

    Clean code
        - modified:   js/app.js
    	- modified:   tasks.md

--- js/app.js
+++ js/app.js
@@ -1,25 +1,28 @@
-/*
- * Create a list that holds all of your cards
- */
 const deck = document.querySelector('.deck');
-//const cards = document.querySelectorAll('.card li');
 
-console.log(deck);
+let openCards = [];
 
-deck.addEventListener('click', function () {
+//console.log(deck);
+
+// listen for click
+deck.addEventListener('click', () => {
     const card = event.target;
     if (validCard) {
         openCloseCard(card);
         console.log(card);
-        return card;
+
+        // return card;
     }
+    playerTurn();
 });
 
-// don't change classes if not card
+
+// don't change classes if not .card
 function validCard(card) {
     card.classList.contains('card');
 }
 
+// toggle cards
 function openCloseCard(card) {
     if (card.classList.contains('card')) {
         card.classList.toggle('open');
@@ -27,42 +30,25 @@ function openCloseCard(card) {
     }
 }
 
-
-/*
-// iterate through cards
-for (let card of cards) {
-    // do something
-    Array.from(card);
-    // tests
-    //console.log(card);
+function playerTurn(card) {
+    if (openCards.length < 2) {
+        openCards.push(card);
+    } else {
+        openCards = [];
+    }
 }
-*/
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
-/*
- - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) */
+console.log(openCards);
 
-/* function openCardList() {
-    openCards = [];
-    Array.from(card);
-} */
 
-/* set up the event listener for a card. If a card is clicked:
-  - display the card's symbol (put this functionality in another function that you call from this one)
- */
 
-/*  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+
+
+
+
+
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
